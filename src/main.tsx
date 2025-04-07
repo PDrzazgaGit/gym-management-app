9ac09cd6cf6fa75d10f1ce96aee55f4ef0838a3d
PDrzazgaGit <151642787+PDrzazgaGit@users.noreply.github.com>
2025-04-07 14:10:13 +0200

    TypeORM installation

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "reflect-metadata"
 
 const root = createRoot(document.body);
 root.render(<h2>Hello from React!</h2>);
